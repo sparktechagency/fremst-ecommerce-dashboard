a3ee6d4bbc8999c39eb852bd2df97ac1ce0c4f10
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-12-31 15:48:38 +0600

    chore: Update Vite dev server host to `164.90.205.5` and port to `4174`.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "164.90.205.5",
-    // port: 4174,
-    host: "10.10.7.47",
-    port: 3005,
+    host: "164.90.205.5",
+    port: 4174,
+    // host: "10.10.7.47",
+    // port: 3000,
   },
 });
